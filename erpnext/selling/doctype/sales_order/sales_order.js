11085059e82c7f3266978d3a9113a177d9213b87
Florian HENRY <florian.henry@open-concept.pro>
2023-11-07 14:37:58 +0100

    on going

--- erpnext/selling/doctype/sales_order/sales_order.js
+++ erpnext/selling/doctype/sales_order/sales_order.js
@@ -94,6 +94,12 @@ frappe.ui.form.on("Sales Order", {
 						frm.set_value("reserve_stock", 0);
 						frm.set_df_property("reserve_stock", "read_only", 1);
 						frm.set_df_property("reserve_stock", "hidden", 1);
+						frm.fields_dict.items.grid.update_docfield_property(
+								'reserve_stock', 'hidden', 1
+						);
+						frm.fields_dict.items.grid.update_docfield_property(
+								'reserve_stock', 'default', 0
+						);
 					}
 				})
 			}
@@ -427,23 +433,28 @@ frappe.ui.form.on("Sales Order", {
 });
 
 frappe.ui.form.on("Sales Order Item", {
-	item_code: function(frm,cdt,cdn) {
+	item_code: async function(frm,cdt,cdn) {
 		var row = locals[cdt][cdn];
-		console.log(cdt,cdn);
 		if (frm.doc.delivery_date) {
 			row.delivery_date = frm.doc.delivery_date;
 			refresh_field("delivery_date", cdn, "items");
 		} else {
 			frm.script_manager.copy_from_first_row("items", row, ["delivery_date"]);
 		}
-		var item = frappe.get_doc("Item", row.item_code);
-		console.log(item);
-		if (!frm.doc.reserve_stock) {
 
-			row.reserve_stock = frm.doc.reserve_stock;
-			row.set_df_property("reserve_stock", "hidden", 1);
-			refresh_field("reserve_stock", cdn, "items");
+		let item = await frappe.db.get_doc("Item", row.item_code);
+		console.log(frm.fields_dict.items);
+		if (!item.is_stock_item) {
+			frm.fields_dict.items.grid.update_docfield_property(
+						'reserve_stock', 'hidden', 1
+			);
+		} else {
+			frm.fields_dict.items.grid.update_docfield_property(
+						'reserve_stock', 'hidden', 0
+			);
 		}
+		refresh_field("reserve_stock", cdn, "items");
+		frm.fields_dict.items.grid.refresh();
 	},
 	delivery_date: function(frm, cdt, cdn) {
 		if(!frm.doc.delivery_date) {
